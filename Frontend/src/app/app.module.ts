6510b3bbcf5a8a5979feb0fb5ea59a366e663ef0
quantranminh <minhquan2122000@gmail.com>
2023-07-10 18:56:13 +0700

    complete keycloak angular

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -8,17 +8,20 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UsersModule } from './components/users/users.module';
 import { CommonsModule } from './components/commons/commons.module';
-import { AuthModule } from './components/auth/auth.module';
 import { RouterModule } from '@angular/router';
 import { AdminsModule } from './components/admins/admins.module';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { MockService } from './shared/mock.service';
 import { SharedModule } from './shared/shared.module';
+import {
+  AbstractSecurityStorage,
+  AuthModule,
+  LogLevel,
+} from 'angular-auth-oidc-client';
+import { LocalStorageService } from './shared/services/local-storage.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -28,13 +31,33 @@ import { SharedModule } from './shared/shared.module';
     FormsModule,
     SharedModule,
     UsersModule,
-    AuthModule,
     AdminsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    AuthModule.forRoot({
+      config: {
+        configId: 'book-store',
+        authority: 'http://localhost:8080/realms/quan-realm',
+        redirectUrl: 'http://localhost:4200/',
+        postLogoutRedirectUri: 'http://localhost:4200/',
+        clientId: 'book-store',
+        scope: 'openid profile email offline_access', // 'openid profile ' + your scopes
+        responseType: 'code',
+        silentRenew: true,
+        useRefreshToken: true,
+        storage: new LocalStorageService(),
+        customParamsAuthRequest: {
+          client_secret: 'm5fGtGGnKoiXcLBhYDdLto3qIZlvAHV8',
+        },
+        customParamsCodeRequest: {
+          client_secret: 'm5fGtGGnKoiXcLBhYDdLto3qIZlvAHV8',
+        },
+      },
+    }),
   ],
   providers: [
+    { provide: AbstractSecurityStorage, useClass: LocalStorageService },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
